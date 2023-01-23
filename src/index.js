3fe1d57f674b692366c8087f83cef668f51b3689
Bleckfi <pashkovskiy.serge@gmail.com>
2023-01-23 20:37:33 +0300

    sol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var i,
+    l = str.length,
+    char,
+    last,
+    stack = [];
+
+  for (i = 0; i < l; i++) {
+    char = str[i];
+
+    if (char == '{' || char == '(') {
+      stack.push(char);
+    } else if (char == '}' || char == ')') {
+      if (stack.length > 0) {
+        last = stack[stack.length - 1];
+        if ((char == '}' && last == '{') || (char == ')' && last == '(')) {
+          stack.pop();
+        }
+      }
+    }
+  }
+  return stack.length == 0;
+};
